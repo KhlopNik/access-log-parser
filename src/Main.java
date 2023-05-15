3cef62c891227a619929cd6f31dc1b812035e192
Nikita Khlopotov <KhlopotovNS@ya.ru>
2023-05-15 22:15:09 +0300

    added sum, difference, multiplication and division of numbers

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+        System.out.println("Введите первое число: ");
+        int firstNumber = Integer.parseInt(new Scanner(System.in).nextLine());
+
+        System.out.println("Введите второе число: ");
+        int secondNumber = Integer.parseInt(new Scanner(System.in).nextLine());
+
+        int sum = firstNumber + secondNumber;
+        System.out.println("Сумма чисел: " + sum);
+
+        int diff = firstNumber - secondNumber;
+        System.out.println("Разность чисел: " + diff);
+
+        int multipl = firstNumber * secondNumber;
+        System.out.println("Произведение чисел: " + multipl);
+
+        double div = (double) firstNumber / secondNumber;
+        System.out.println("Частное чисел: " + div);
     }
 }
